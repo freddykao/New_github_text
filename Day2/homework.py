a1efee82e174cc4291116e36593a285fca9b2d61
kaosan <43395676+freddykao@users.noreply.github.com>
2020-06-04 00:53:36 +0800

    Day2

--- Day2/homework.py
+++ Day2/homework.py
@@ -1,10 +1,14 @@
 import cv2 
 import numpy as np 
 image = cv2.imread( "lena.png" , cv2.IMREAD_COLOR)
-cvt  = cv2.cvtColor( image ,cv2.COLOR_BGR2HSV)
+cvt  = cv2.cvtColor( image ,cv2.COLOR_BGR2HLS)
+img_lab = cv2.cvtColor(cvt, cv2.COLOR_BGR2LAB)
+
 while True:
     cv2.imshow("cvt " , cvt)
     cv2.imshow("Image",image)
+    cv2.imshow("img_lab",img_lab)
+
     (B,G,R)=cv2.split(image)
     cv2.imshow('R',R)
     cv2.imshow('G',G)
