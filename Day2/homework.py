d1561fca82191fc466d3558eec1e15f4c7023967
kaosan <43395676+freddykao@users.noreply.github.com>
2020-06-04 00:40:22 +0800

    Day2

--- Day2/homework.py
+++ Day2/homework.py
@@ -5,6 +5,10 @@ cvt  = cv2.cvtColor( image ,cv2.COLOR_BGR2HSV)
 while True:
     cv2.imshow("cvt " , cvt)
     cv2.imshow("Image",image)
+    (B,G,R)=cv2.split(image)
+    cv2.imshow('R',R)
+    cv2.imshow('G',G)
+    cv2.imshow('B',B)
     if cv2.waitKey(0) == 27 :
         cv2.destroyAllWindows()
         break
