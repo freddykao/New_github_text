01ef80034fd6225273313b05b768211bb45e0ff7
kaosan <43395676+freddykao@users.noreply.github.com>
2020-06-10 14:35:11 +0800

    Day4

---
+++ Day4/homework.py
@@ -0,0 +1,107 @@
+import cv2
+import time
+import numpy as np
+
+img = cv2.imread("lena.png")
+
+# img_hflip = 
+
+# 垂直翻轉 (vertical)
+img_vflip = cv2.flip(img,1)
+img_hflip = cv2.flip(img,-1)
+
+cv2.imshow("翻轉",img_vflip)
+cv2.imshow("翻轉",img_hflip)
+
+
+# 水平 + 垂直翻轉
+# img_hvflip = 
+
+# # 組合 + 顯示圖片
+# hflip = np.hstack((img, img_hflip))
+# vflip = np.hstack((img_vflip, img_hvflip))
+# img_flip = np.vstack((hflip, vflip))
+# while True:
+#     cv2.imshow('flip image', img_flip)
+#     k = cv2.waitKey(0)
+#     if k == 27:
+#         cv2.destroyAllWindows()
+#         break
+
+# 為了要改變飽和度，我們先把 color space 轉成 HSL 格式 (OpenCV 表示順序是 HLS)
+img_hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+change_percentage = 0.2
+
+# 針對飽和度的值做改變，超過界線 0~1 的都會 bound
+# 在 HLS color space 減少飽和度
+img_hls_down = img_hls.astype('float32')
+img_hls_down[..., -1] = img_hls_down[..., -1]/255 - change_percentage
+img_hls_down[img_hls_down[..., -1] < 0] = 0
+img_hls_down[..., -1] = img_hls_down[..., -1]*255
+img_hls_down = img_hls_down.astype('uint8')
+
+# 在 HLS color space 增加飽和度
+img_hls_up = img_hls.astype('float32')
+img_hls_up[..., -1] = img_hls_up[..., -1]/255 + change_percentage
+img_hls_up[img_hls_up[..., -1] > 1] = 1
+img_hls_up[..., -1] = img_hls_up[..., -1]*255
+img_hls_up = img_hls_up.astype('uint8')
+
+# 轉換
+img_hls_down = cv2.cvtColor(img_hls_down, cv2.COLOR_HLS2BGR)
+img_hls_up = cv2.cvtColor(img_hls_up, cv2.COLOR_HLS2BGR)
+
+# 組合圖片 + 顯示圖片
+img_hls_change = np.hstack((img, img_hls_down, img_hls_up))
+while True:
+    cv2.imshow('change saturation', img_hls_change)
+    
+    k = cv2.waitKey(0)
+    if k == 27:
+        cv2.destroyAllWindows()
+        break
+
+"""
+直方圖均衡
+
+"""
+
+
+# 轉為灰階圖片
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# 灰階圖片直方圖均衡
+img_gray_equal = cv2.equalizeHist(img_gray)
+
+# 組合圖片 + 顯示圖片
+img_gray_equalHist = np.hstack((img_gray, img_gray_equal))
+while True:
+    # 比較灰階圖片是否有做直方圖均衡的差異
+    cv2.imshow('gray equal histogram', img_gray_equalHist)
+
+    k = cv2.waitKey(0)
+    if k == 27:
+        cv2.destroyAllWindows()
+        break
+
+
+
+"""
+調整對比 / 明亮
+
+
+"""
+# alpha: 控制對比度 (1.0~3.0)
+# beta: 控制明亮度 (0~255)
+add_contrast = cv2.convertScaleAbs(img, alpha=2.0, beta=0)
+add_lighness = cv2.convertScaleAbs(img, alpha=1.0, beta=50)
+
+# 組合圖片 + 顯示圖片
+img_contrast_light = np.hstack((img, add_contrast, add_lighness))
+while True:
+    # 比較不同程度的對比 / 明亮
+    cv2.imshow('adjust contrast and brighness', img_contrast_light)
+    k = cv2.waitKey(0)
+    if k == 27:
+        cv2.destroyAllWindows()
+        break
