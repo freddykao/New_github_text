3da6782c1dd280443c4a6c6171795dc2b6568f82
kaosan <43395676+freddykao@users.noreply.github.com>
2020-06-12 23:46:01 +0800

    Day4

--- Day4/homework.py
+++ Day4/homework.py
@@ -10,97 +10,31 @@ img = cv2.imread("lena.png")
 img_vflip = cv2.flip(img,1)
 img_hflip = cv2.flip(img,-1)
 
-cv2.imshow("翻轉",img_vflip)
-cv2.imshow("翻轉",img_hflip)
+cv2.imshow("img_vflip",img_vflip)
+cv2.imshow("img_hflip",img_hflip)
 
 
-# 水平 + 垂直翻轉
-# img_hvflip = 
 
-# # 組合 + 顯示圖片
-# hflip = np.hstack((img, img_hflip))
-# vflip = np.hstack((img_vflip, img_hvflip))
-# img_flip = np.vstack((hflip, vflip))
-# while True:
-#     cv2.imshow('flip image', img_flip)
-#     k = cv2.waitKey(0)
-#     if k == 27:
-#         cv2.destroyAllWindows()
-#         break
+# 將圖片縮小成原本的 20%
+img_test = cv2.resize(img, None, fx=0.2, fy=0.2)
 
-# 為了要改變飽和度，我們先把 color space 轉成 HSL 格式 (OpenCV 表示順序是 HLS)
-img_hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
-change_percentage = 0.2
+# 將圖片放大為"小圖片"的 8 倍大 = 原圖的 1.6 倍大
+fx, fy = 8, 8
 
-# 針對飽和度的值做改變，超過界線 0~1 的都會 bound
-# 在 HLS color space 減少飽和度
-img_hls_down = img_hls.astype('float32')
-img_hls_down[..., -1] = img_hls_down[..., -1]/255 - change_percentage
-img_hls_down[img_hls_down[..., -1] < 0] = 0
-img_hls_down[..., -1] = img_hls_down[..., -1]*255
-img_hls_down = img_hls_down.astype('uint8')
+# 鄰近差值 scale + 計算花費時間
+start_time = time.time()
+img_area_scale = 
+print('INTER_NEAREST zoom cost {}'.format(time.time() - start_time))
 
-# 在 HLS color space 增加飽和度
-img_hls_up = img_hls.astype('float32')
-img_hls_up[..., -1] = img_hls_up[..., -1]/255 + change_percentage
-img_hls_up[img_hls_up[..., -1] > 1] = 1
-img_hls_up[..., -1] = img_hls_up[..., -1]*255
-img_hls_up = img_hls_up.astype('uint8')
+# 雙立方差補 scale + 計算花費時間
+start_time = time.time()
+img_cubic_scale = 
+print('INTER_CUBIC zoom cost {}'.format(time.time() - start_time))
 
-# 轉換
-img_hls_down = cv2.cvtColor(img_hls_down, cv2.COLOR_HLS2BGR)
-img_hls_up = cv2.cvtColor(img_hls_up, cv2.COLOR_HLS2BGR)
-
-# 組合圖片 + 顯示圖片
-img_hls_change = np.hstack((img, img_hls_down, img_hls_up))
+# 組合 + 顯示圖片
+img_zoom = np.hstack((img_area_scale, img_cubic_scale))
 while True:
-    cv2.imshow('change saturation', img_hls_change)
-    
-    k = cv2.waitKey(0)
-    if k == 27:
-        cv2.destroyAllWindows()
-        break
-
-"""
-直方圖均衡
-
-"""
-
-
-# 轉為灰階圖片
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# 灰階圖片直方圖均衡
-img_gray_equal = cv2.equalizeHist(img_gray)
-
-# 組合圖片 + 顯示圖片
-img_gray_equalHist = np.hstack((img_gray, img_gray_equal))
-while True:
-    # 比較灰階圖片是否有做直方圖均衡的差異
-    cv2.imshow('gray equal histogram', img_gray_equalHist)
-
-    k = cv2.waitKey(0)
-    if k == 27:
-        cv2.destroyAllWindows()
-        break
-
-
-
-"""
-調整對比 / 明亮
-
-
-"""
-# alpha: 控制對比度 (1.0~3.0)
-# beta: 控制明亮度 (0~255)
-add_contrast = cv2.convertScaleAbs(img, alpha=2.0, beta=0)
-add_lighness = cv2.convertScaleAbs(img, alpha=1.0, beta=50)
-
-# 組合圖片 + 顯示圖片
-img_contrast_light = np.hstack((img, add_contrast, add_lighness))
-while True:
-    # 比較不同程度的對比 / 明亮
-    cv2.imshow('adjust contrast and brighness', img_contrast_light)
+    cv2.imshow('zoom image', img_zoom)
     k = cv2.waitKey(0)
     if k == 27:
         cv2.destroyAllWindows()
